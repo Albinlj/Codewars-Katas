88929e26437665e9b209a994b4c78e17dd835690
Albin Ljunghusen <albinljunghusen@gmail.com>
2018-05-01 17:03:04 +0200

    Niceare

--- WriteNumberInExpandedForm.js
+++ WriteNumberInExpandedForm.js
@@ -1,14 +1,10 @@
 function expandedForm(num) {
-    var arr = ('' + num).split('');
-    var newArr = [];
-    for (i = 0; i < arr.length; i++) {
-        element = arr[i];
-        if (element != 0) {
-            newElement = element * Math.pow(10, (arr.length - i - 1));
-            newArr.push(newElement);
-        }
-    }
-    return newArr.join(' + ');
+    return num.toString()
+        .split('')
+        .map((element, index, array) => element * Math.pow(10, (array.length - index - 1)))
+        .filter(a => a > 0)
+        .join(' + ')
+
 }
 
 console.log(expandedForm(9087));
