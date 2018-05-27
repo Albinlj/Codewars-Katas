e7748db918a291853e533e016df464a3c2581999
Albinlj <albinljunghusen@gmail.com>
2018-05-27 21:32:36 +0200

    Refactored Land Perimeter

--- Land perimeter.js
+++ Land perimeter.js
@@ -3,36 +3,12 @@ function landPerimeter(arr) {
         return borderTotal += string.split("").reduce((stringBorderTotal, letter, x) => {
             var bordersToAdd = 0;
             if (letter == "X") {
-                bordersToAdd += 4;
-                if (x - 1 >= 0 && string.substr(x - 1, 1) == "X") {
-                    bordersToAdd--;
-                }
-                if (x + 1 < string.length && string.substr(x + 1, 1) == "X") {
-                    bordersToAdd--;
-                }
-                if (y - 1 >= 0 && arr[y - 1].substr(x, 1) == "X") {
-                    bordersToAdd--;
-                }
-                if (y + 1 < arr.length && arr[y + 1].substr(x, 1) == "X") {
-                    bordersToAdd--;
-                }
+                if (x == 0 || string.substr(x - 1, 1) == "O") bordersToAdd++;
+                if (x == string.length - 1 || string.substr(x + 1, 1) == "O") bordersToAdd++;
+                if (y == 0 || arr[y - 1].substr(x, 1) == "O") bordersToAdd++;
+                if (y == arr.length - 1 || arr[y + 1].substr(x, 1) == "O") bordersToAdd++;
             }
             return stringBorderTotal + bordersToAdd;
         }, 0)
     }, 0);
 }
-
-console.log(landPerimeter([
-    'XOOXO',
-    'XOOXO',
-    'OOOXO',
-    'XXOXO',
-    'OXOOO'
-]))
-console.log(landPerimeter([
-    'XOOO',
-    'XOXO',
-    'XOXO',
-    'OOXX',
-    'OOOO'
-]))
