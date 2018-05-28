ba08a307e16419001946f204b1c154e07663d95c
Albinlj <albinljunghusen@gmail.com>
2018-05-28 21:45:01 +0200

    Failed and then succeeded with help

--- Best travel.js
+++ Best travel.js
@@ -1,44 +1,55 @@
 function chooseBestSum(t, k, ls) {
-    let options = [];
+    var highestSum = null;
 
-
-    function getOptions(arr, journeys) {
-        if (arr.length > ls.length - k) {
-            var newJourneys = [];
-            if (journeys.length > 0) {
-                journeys.reduce(function (total, journey, index) {
-                    console.log("at journey " + journey + " with index " + index);
-                    arr.reduce(function (total, route, index) {
-                        console.log("at route " + route + " with index " + index);
-                        newJourneys.push(journey.concat(route));
-                        let splicedArr = arr;
-                        newJourneys = [splicedArr.splice(index, 1)];
-                        return getOptions(splicedArr, newJourneys);
-                    })
-                })
-            } else {
-                arr.reduce(function (total, route, index) {
-                    let splicedArr = arr;
-                    newJourneys = [splicedArr.splice(index, 1)];
-                    return getOptions(splicedArr, newJourneys);
-                })
-            }
-        } else {
-            return journeys;
+    function townRecurse(townsSoFar, lastindex) {
+        var townsSoFar = townsSoFar || [];
+        if (townsSoFar.length === k) {
+            let sum = townsSoFar.reduce((a, b) => a + b)
+            if (sum < t && sum > highestSum)
+                highestSum = sum;
+            return;
         }
-
+        for (var i = lastindex + 1 || 0; i < ls.length; i++) {
+            townRecurse(townsSoFar.concat(ls[i]), i);
+        }
     }
 
-    return getOptions(ls, []);
+
+    townRecurse();
+    return highestSum
 }
 
-// function getAllOptions(arr, options) {
-//     if (arr.length > ls.length - k) {
-//         return arr.reduce(function (total, value) {
-//             valueArr = [value];
-//             return total.concat(getAllOptions(valueArr));
-//         })
-//     } else {
-//         return (arr);
+
+
+// My failed attempt at recursion. After reading another users solution i attempted to recreate it above. 
+
+// function chooseBestSum(t, k, ls) {
+//     var origLen = ls.length;
+
+//     function getOptions(arr, journeys) {
+//         if (arr.length > origLen - k) {
+//             var newJourneys = [];
+//             if (journeys.length > 0) {
+//                 return journeys.reduce(function (total, journey, index) {
+//                     return total + arr.reduce(function (total, route, index) {
+//                         total.push(journey.concat(route));
+//                         return total;
+//                     }, [])
+//                 }, null)
+//             } else {
+//                 arr.reduce(function (total, route, index) {
+//                     let splicedArr = arr;
+//                     newJourneys = [splicedArr.splice(index - 1, 1)];
+//                     return total + getOptions(splicedArr, newJourneys);
+//                 })
+//             }
+//         } else {
+//             return journeys;
+//         }
+
 //     }
-console.log(chooseBestSum(100, 2, [5, 6, 7]));
+
+//     return getOptions(ls, []);
+// }
+
+console.log(chooseBestSum(14, 5, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
